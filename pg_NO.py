75657227f4587b47963b0d9a82ecbbd48b98d7cf
Margot53 <33633179+Margot53@users.noreply.github.com>
2018-01-08 11:34:07 -0500

    Add files via upload

---
+++ pg_NO.py
@@ -0,0 +1,10 @@
+import pyautogui as pg
+
+pg.hotkey('ctrl', 'winleft', 'd')
+pg.hotkey('winleft')
+pg.typewrite('chrome/n',.3)
+
+pg.hotkey('winleft', 'up')
+pg.typewrite('espn.com/n,.3)
+
+             
